51fa85c845ba236005b404cff0ed5503c1c030f5
chskTg <beingmsd77@gmail.com>
2024-03-28 22:03:29 -0400

    simple fe design

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("run-query").addEventListener("click", function () {
+  var query = document.getElementById("sql-query").value;
+  fetch("/query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query: query }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const resultElement = document.getElementById("query-result");
+      resultElement.innerHTML =
+        "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
